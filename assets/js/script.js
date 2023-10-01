f05c38b22a9d9b901f7c03f6357fa8ce396ddee0
Katherine <kfmatovic@icloud.com>
2023-09-30 17:10:59 -0700

    add quiz array and for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,10 @@
 var timer = document.querySelector(".time");
 var mainEl = document.getElementById("main");
 var startButtn = document.querySelector(".start");
+var pargElmt = document.querySelector("p");
 
-var secondsLeft = 61;
+
+var secondsLeft = 76;
 
 function setTime() {
   // Sets interval in variable
@@ -26,8 +28,49 @@ function setTime() {
   }, 1000);
 }
 
-
-
+/* add event listener for start button; when user clicks, timer starts
+and series of questions pop-up to be answered */
 startButtn.addEventListener("click", function() {
     setTime();
+    // var h1Elmt = document.queryselector("h1");
+    mainEl.textContent = " ";
+    // h1Elmt.textContent = " ";
+    // startButtn.textContent =" ";
+
 })
+
+var score = 0;
+
+var quizzes = [ 
+    {
+        question: "Commonly used data types DO not include: \n (a) strings \n (b) booleans \n (c) alerts \n (d) numbers",
+        answer: "c"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with _____. \n (a) quotes \n (b)curly brackets \n (c) parenthesis \n (d) square brackets",
+        answer: "b"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ______. \n (a) numbers and strings \n (b) other arrays \n (c)booleans \n (d) all of the above",
+        answer: "d"
+    },
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables. \n (a) commas, (b) curly brackets (c) quotes (d) parenthesis",
+        answer: "c"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is: \n (a) JavaScript (b) terminal/bash (c) for loops (d)console.log ",
+        answer: "d"
+    }]
+
+for (q=0; q < quizzes.length; q++) {
+    var userAns = window.prompt(quizzes[q].answer);
+    
+    if (userAns == quizzes[i].answer) {
+        score++;
+        alert("Correct ✅");
+    } else {
+        score
+        alert("Wrong ❌");
+    }
+}
